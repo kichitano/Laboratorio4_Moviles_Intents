37a419a37827a41e2e93f38005d8548fa004b360
kichitano <30935429+kichitano@users.noreply.github.com>
2019-09-03 20:52:40 -0500

    A la mitad de lab

--- app/src/main/java/com/example/intents_android/MainActivity.java
+++ app/src/main/java/com/example/intents_android/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.intents_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int CAMERA_PIC_REQUEST = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn_llamar = findViewById(R.id.btn_call);
         Button btn_location = findViewById(R.id.btn_location);
+        Button btn_web = findViewById(R.id.btn_web);
+        Button btn_foto = findViewById(R.id.btn_foto);
 
         btn_llamar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +46,47 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(mapIntent);
             }
         });
+
+        btn_web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri webpage = Uri.parse("http://upt.edu.pe");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                PackageManager packageManager = getPackageManager();
+                List<ResolveInfo> activities = packageManager.queryIntentActivities(webIntent, packageManager.MATCH_DEFAULT_ONLY);
+
+                boolean isIntentSafe = activities.size() > 0;
+                String titulo = getResources().getString(R.string.chooser_title);
+                Intent wChooser = Intent.createChooser(webIntent, titulo);
+
+                if(isIntentSafe){
+                    startActivity(wChooser);
+                }
+
+            }
+        });
+
+        btn_foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
+            }
+        });
+
+
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == CAMERA_PIC_REQUEST){
+            if(resultCode == RESULT_OK){
+                Bitmap imagen = (Bitmap)data.getExtras().get("data");
+                ImageView foto = findViewById(R.id.imgFoto);
+                foto.setImageBitmap(imagen);
+            }
+        }
+    }
+
 }
